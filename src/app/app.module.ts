4a2c0cb3784e59c9d210e9b6a161727f3620dbe4
pedromellodev <pedromello19122006@gmail.com>
2022-04-13 16:35:09 -0300

    buttons component commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
